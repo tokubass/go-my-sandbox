e72aae6297e96d9cec25ed244a3ae392128da381
tokubass <tokubass@cpan.org>
2019-01-11 01:13:12 +0900

    check interface

--- mock_with_build_tag/client/mock_client_lock.go
+++ mock_with_build_tag/client/mock_client_lock.go
@@ -4,12 +4,35 @@ package client
 
 import (
 	"my/go-my-sandbox/mock_with_build_tag/domain"
+	"reflect"
 	"sync"
 )
 
-// 並列処理で書き換えられると困るのでlock
+type C struct {
+	domain.C
+	MockTag func() string
+}
+
+// mockがdomainのinterfaceから乖離していないかチェック
+func init() {
+	c := &C{}
+	if reflect.TypeOf(c.Tag) != reflect.TypeOf(c.MockTag) {
+		panic("mock interface should be the same as domain interface")
+	}
+}
+
 var defaultMockTag = func() string { return "use_lock" }
 var mockTag = defaultMockTag
+
+func NewClient() *C {
+	return &C{MockTag: mockTag}
+}
+
+func (c *C) Tag() string {
+	return c.MockTag()
+}
+
+// 並列処理で書き換えられると困るのでlock
 var mutex sync.Mutex
 
 func Set_Client_MockTag(f func() string) func() error {
@@ -21,17 +44,4 @@ func Set_Client_MockTag(f func() string) func() error {
 		mutex.Unlock()
 		return nil
 	}
-}
-
-type C struct {
-	domain.C
-	MockTag func() string
-}
-
-func NewClient() *C {
-	return &C{MockTag: mockTag}
-}
-
-func (c *C) Tag() string {
-	return c.MockTag()
 }
